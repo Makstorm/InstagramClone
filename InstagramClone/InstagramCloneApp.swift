d0c501a15164232d5f6581c895fd6f7ea217ab34
makstorm <mxm.horobets@gmail.com>
2026-04-15 18:21:13 +0200

    Initial Commit

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Maxym Horobets on 15.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
